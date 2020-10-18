3dd04b6e7b9f8ed4570fd1233a33da7ed1a852bb
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2020-10-18 21:42:41 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,52 +1,41 @@
 from easydict import EasyDict as edict
-import json
 from os.path import expanduser
 
 config = edict()
-config.TRAIN = edict()
-config.VALID = edict()
 
-# home path
-config.home_path = expanduser("~")
+# Home Path
+config.homePath = expanduser("~")
 
-# checkpoint location
-config.checkpoint_path = config.home_path + '/SRNet-D/checkpoint/'
+# Checkpoint Location
+config.checkpointPath = config.homePath + '/SRNet-D/checkpoint/'
 
-# samples location
-config.samples_path = config.home_path + '/SRNet-D/samples/'
+# Samples Location
+config.samplesPath = config.homePath + '/SRNet-D/samples/'
 
-# save file format
-config.save_file_format = '.png'
+# Save File Format
+config.saveFileFormat = '.png'
 
-# input file format
-config.input_img_name_regx = '.*\.(bmp|png|webp|jpg)'
+# Batch
+config.miniBatchSize = 2
 
 # Adam
-config.TRAIN.sample_batch_size = 25
-config.TRAIN.batch_size = 2
-config.TRAIN.learning_rate = 1e-4
+config.learningRate = 2e-5
 
-# training
-config.TRAIN.n_epoch = 400
+# Training
+config.nEpoch = 100
 
-# noise reduction
+# Noise Reduction
 # WebP compression level; 1 to 100 (smaller value adds more noise)
-config.TRAIN.noise_level = 10
+config.noiseLevel = 10
 
-# train set location
-config.TRAIN.hr_img_path = config.home_path + '/SRNet-D/HRImage_Training/'
+# Validation Set Location
+config.validationHRImagePath = config.homePath + '/SRNet-D/HRImage-Validation/'
 
-# validation set location
-config.VALID.hr_img_path = config.home_path + '/SRNet-D/HRImage_Validation/'
+# Train Set Location
+config.trainingHRImagePath = config.homePath + '/SRNet-D/HRImage-Training/'
 
-# test set location
-config.VALID.eval_img_path = config.home_path + '/SRNet-D/LRImage_Evaluation/'
+# Test Set Location
+config.evaluationImagePath = config.homePath + '/SRNet-D/LRImage-Evaluation/'
 
-# enlargement set location
-config.VALID.enlargement_lr_img_path = config.home_path + '/SRNet-D/LRImage_Enlargement/'
-
-def log_config(filename, cfg):
-    with open(filename, 'w') as f:
-        f.write("================================================\n")
-        f.write(json.dumps(cfg, indent=4))
-        f.write("\n================================================\n")
+# Tnlargement Set Location
+config.enlargementLRImagePath = config.homePath + '/SRNet-D/LRImage-Enlargement/'
