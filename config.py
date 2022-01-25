18248f7ebf276419282c8a5c8698dfd5e3fec777
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2022-01-25 18:32:59 +0900

    Update config.py

--- config.py
+++ config.py
@@ -18,18 +18,17 @@ config.saveFileFormat = '.png'
 # Batch
 config.miniBatchSize = 1
 
-# Adam
-config.learningRate = 2e-5
+# Learning Rate of RAdam
+config.learningRate = 5e-6
+
+# Weight Decay of RAdam
+config.weightDecay = 1e-5
 
 # Training
 config.nEpoch = 800
 
 # Number of Iterations of the Step to Save
-config.nIterationOfStepToSave = 4000
-
-# Noise Reduction
-# WebP compression level; 1 to 100 (smaller value adds more noise)
-config.noiseLevel = 5
+config.nIterationOfStepToSave = 1000
 
 # Validation Set Location
 config.validationHRImagePath = config.homePath + '/SRNet-DR/HRImage-Validation/'
@@ -37,8 +36,5 @@ config.validationHRImagePath = config.homePath + '/SRNet-DR/HRImage-Validation/'
 # Train Set Location
 config.trainingHRImagePath = config.homePath + '/SRNet-DR/HRImage-Training/'
 
-# Test Set Location
-config.evaluationImagePath = config.homePath + '/SRNet-DR/LRImage-Evaluation/'
-
-# Tnlargement Set Location
+# Enlargement Set Location
 config.enlargementLRImagePath = config.homePath + '/SRNet-DR/LRImage-Enlargement/'
