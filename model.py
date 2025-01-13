88a8b50799b8e668d3cc9fceb1027d5307715319
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2025-01-13 16:36:26 +0900

    Update model.py

--- model.py
+++ model.py
@@ -56,14 +56,13 @@ class TanhExp(torch.nn.Module):
 class ModelOfGenerator(torch.nn.Module):
     def __init__(self):
         super().__init__() # 親クラスである torch.nn.Module の __init__ を継承する。
-        # weightの初期化はKaiming Heの初期化で自動的に成される。
+        # weightの既定の初期化はKaiming Heの初期化。
 
-        numberOfChannels1 = 480 # 480
-        numberOfChannels2 = 384 # 384
-        numberOfGroups1 = 1
-        numberOfGroups2 = 1
-        self.numberOfResidualBlocks1 = 8 # 8
-        self.numberOfResidualBlocks2 = 16 # 16
+        numberOfChannels1 = 64 # recommended value: 480
+        numberOfChannels2 = 64 # recommended value: 384
+        numberOfGroups = 1
+        self.numberOfResidualBlocks1 = 8 # recommended value: 20
+        self.numberOfResidualBlocks2 = 8 # recommended value: 8
         index = 0
         layersList = []
 
@@ -75,6 +74,8 @@ class ModelOfGenerator(torch.nn.Module):
             torch.nn.Conv2d(in_channels=3 * 4 * 4, out_channels=numberOfChannels1, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=1, bias=False, padding_mode='replicate'))
 
         # Normalizationは入力データを平均0、標準偏差1となるように変換する。従って、出力には負の値及び1.0より大きな値を含む。これにより入力データの情報利用効率を上げる。
+        # Normalizationは学習可能なパラメーターgamma, betaを持つ。gammaはスケール、betaはバイアスである。
+        # affine=Trueでパラメーターを学習する。この時、直前の層ではbias=Falseとする。
         layersList.append(
             torch.nn.GroupNorm(num_groups=int(numberOfChannels1 / (3 * 4 * 4)), num_channels=numberOfChannels1, eps=1e-05, affine=True))
 
@@ -82,25 +83,25 @@ class ModelOfGenerator(torch.nn.Module):
 
 
         # Residual Blocks
-        for j in range(self.numberOfResidualBlocks2):
-            for i in range(self.numberOfResidualBlocks1):
+        for i in range(self.numberOfResidualBlocks1):
+            for j in range(self.numberOfResidualBlocks2):
                 layersList.append(
-                    torch.nn.Conv2d(in_channels=numberOfChannels1, out_channels=numberOfChannels2, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=numberOfGroups1, bias=True, padding_mode='replicate'))
+                    torch.nn.Conv2d(in_channels=numberOfChannels1, out_channels=numberOfChannels2, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=numberOfGroups, bias=True, padding_mode='replicate'))
 
                 layersList.append(TanhExp())
 
                 layersList.append(
-                    torch.nn.Conv2d(in_channels=numberOfChannels2, out_channels=numberOfChannels1, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=numberOfGroups2, bias=True, padding_mode='replicate'))
+                    torch.nn.Conv2d(in_channels=numberOfChannels2, out_channels=numberOfChannels1, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=numberOfGroups, bias=True, padding_mode='replicate'))
 
                 layersList.append(TanhExp())
 
             layersList.append(
-                torch.nn.Conv2d(in_channels=numberOfChannels1, out_channels=numberOfChannels1, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=1, bias=True, padding_mode='replicate'))
+                torch.nn.Conv2d(in_channels=numberOfChannels1, out_channels=numberOfChannels1, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=numberOfGroups, bias=True, padding_mode='replicate'))
 
             layersList.append(TanhExp())
 
         layersList.append(
-            torch.nn.Conv2d(in_channels=numberOfChannels1, out_channels=numberOfChannels1, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=1, bias=True, padding_mode='replicate'))
+            torch.nn.Conv2d(in_channels=numberOfChannels1, out_channels=numberOfChannels1, kernel_size=(3, 3), stride=1, padding=1, dilation=1, groups=numberOfGroups, bias=True, padding_mode='replicate'))
 
         layersList.append(TanhExp())
         # Residual Blocks end
@@ -127,6 +128,7 @@ class ModelOfGenerator(torch.nn.Module):
             torch.nn.Conv2d(in_channels=int(512 / 4), out_channels=3, kernel_size=(1, 1), stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='replicate'))
 
         self.layers = torch.nn.ModuleList(layersList)
+        self.layers.apply(self._initializeWeights)
 
 
     def forward(self, x):
@@ -146,10 +148,10 @@ class ModelOfGenerator(torch.nn.Module):
 
 
             # Residual Blocks
-            x1 = x
-            for k in range(self.numberOfResidualBlocks2):
-                x0 = x
-                for j in range(self.numberOfResidualBlocks1):
+            x0 = x
+            for j in range(self.numberOfResidualBlocks1):
+                x1 = x
+                for j in range(self.numberOfResidualBlocks2):
                     h = self.layers[i](x) # Conv2d
                     i += 1
                     h = self.layers[i](h) # TanhExp
@@ -163,12 +165,12 @@ class ModelOfGenerator(torch.nn.Module):
                 i += 1
                 x = self.layers[i](x) # TanhExp
                 i += 1
-                x = x + x0
+                x = x + x1
             x = self.layers[i](x) # Conv2d
             i += 1
             x = self.layers[i](x) # TanhExp
             i += 1
-            x = x + x1
+            x = x + x0
             # Residual Blocks end
 
 
@@ -190,3 +192,10 @@ class ModelOfGenerator(torch.nn.Module):
             x = x + xOrigin
 
             return x
+
+
+    def _initializeWeights(self, m):
+        if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
+             #torch.nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5), mode='fan_in', nonlinearity='leaky_relu', generator=None) # default.
+             torch.nn.init.kaiming_uniform_(m.weight, a=math.sqrt(16), mode='fan_in', nonlinearity='leaky_relu', generator=None)
+             #m.bias.data.fill_(0.01)
